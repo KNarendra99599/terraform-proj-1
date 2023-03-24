b7cdf46d43f5bc843649843839238c022fd57a4c
root <you@example.com>
2023-03-24 11:31:58 +0530

    provider.tf and vars.tf

---
+++ vars.tf
@@ -0,0 +1,21 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "ZONE1" {
+  default = "us-east-1c"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-0557a15b87f6559cf"
+    us-east-2 = "ami-00eeedc4036573771"
+  }
+
+}
+
+variable "USER" {
+  default = "ec2-user"
+
+}
